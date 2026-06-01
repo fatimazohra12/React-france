179fa71e4f6456d808ffe95e0b8ce80f43bdd7f7
FATIMA ZOHRA AMEJOUD <94472458+fatimazohra12@users.noreply.github.com>
2026-06-01 22:02:38 +0100

    change 1

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,7 +153,7 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+            &copy; 2026 React Land Page Template. Design by{" "}
             <a href="http://www.templatewire.com" rel="nofollow">
               TemplateWire
             </a>
